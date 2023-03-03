ad82bdc9a4edc655fdf5a639fa27872e80bdf0ca
janver426 <123877282+janver426@users.noreply.github.com>
2023-03-03 22:21:15 +0800

    Delete Modify.sh

--- diy/Modify.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# --------------------------------------------------------
-# Script for creating ACL file for each LuCI APP
-rm -rf ./*/.git & rm -f ./*/.gitattributes
-rm -rf ./*/.svn & rm -rf ./*/.github & rm -rf ./*/.gitignore
-rm -rf create_acl_for_luci.err & rm -rf create_acl_for_luci.ok
-rm -rf create_acl_for_luci.warn
-
-exit 0
